6e7a06ae7ffcb443e1cfccd1b20c844277f76e65
Gabriel Tonete <gabtopi77@gmail.com>
2022-03-11 19:15:13 -0300

    port and procfile

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,9 @@ require('dotenv').config({
     path: resolve(__dirname, '..', '.env')
 });
 
-const PORT = process.env.PORT;
+//const PORT = process.env.PORT;
+
+const { PORT }  = process.env;
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
