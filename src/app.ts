246e385d8e5d8f8288f055f3ec1c7659d62d1584
Gabriel Tonete <gabtopi77@gmail.com>
2022-03-11 21:16:01 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -3,29 +3,12 @@ require('dotenv').config({
 });
 import express from 'express';
 import { router } from './routes/routes';
-import cors from 'cors';
 import { resolve } from 'path';
 
 const app = express();
 
-const options: cors.CorsOptions = {
-  methods: "OPTIONS, POST",
-  origin: "https://frontend-newsletter-nextjs.vercel.app/",
-  optionsSuccessStatus: 200
-};
-
-app.use(cors())
-
 app.use(express.json());
 
 app.use(router);
-
-
-
-const { PORT }  = process.env;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`)
-})
 
 export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-/*import { app } from './app';
+import { app } from './app';
 import { resolve } from 'path';
+import cors from 'cors';
 
 require('dotenv').config({
     path: resolve(__dirname, '..', '.env')
 });
 
+app.use(cors())
 
 const { PORT }  = process.env;
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
-})*/
+})
