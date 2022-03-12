f27b2695da081525308c3af79730e48986455a45
Gabriel Tonete <gabtopi77@gmail.com>
2022-03-11 21:24:54 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,24 @@
-require('dotenv').config({
-    path: resolve(__dirname, '..', '.env')
-});
+
 import express from 'express';
 import { router } from './routes/routes';
-import { resolve } from 'path';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+    origin: ['*'],
+    methods: ['*'],
+    optionsSuccessStatus: 200
+}))
 
 app.use(express.json());
 
 app.use(router);
+
+const { PORT }  = process.env;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
 
 export { app };
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,7 @@
-import { app } from './app';
-import { resolve } from 'path';
-import cors from 'cors';
-
-require('dotenv').config({
-    path: resolve(__dirname, '..', '.env')
-});
-
-app.use(cors({
-    origin: ['*'],
-    methods: ['*'],
-    optionsSuccessStatus: 200
-}))
+/*import { app } from './app';
 
 const { PORT }  = process.env;
 
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT}`)
-})
+})*/
