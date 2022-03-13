283a83eb18196b88e77157bc9fbdce131f62455b
Gabriel Tonete <gabtopi77@gmail.com>
2022-03-12 23:34:09 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,12 @@ import { cors } from './services/cors';
 
 const app = express();
 
-app.use(cors);
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 app.use(router);
 
