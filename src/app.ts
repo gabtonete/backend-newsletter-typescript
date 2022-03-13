250dab413afcf3db2bb88e6ee4193586d5e93ad9
Gabriel Tonete <gabtopi77@gmail.com>
2022-03-12 22:53:37 -0300

    [Fix] Cors middleware update

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,9 @@ import cors from 'cors';
 
 const app = express();
 
-app.use(cors());
+app.use(router);
 
 app.use(express.json());
-
-app.use(router);
 
 app.listen(5000, () => {
     console.log("Listening on port 5000")
