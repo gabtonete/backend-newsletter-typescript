38598ec58b8b0e61fa59f885bcd9b920f9c36979
Gabriel Tonete <gabtopi77@gmail.com>
2022-03-12 23:43:21 -0300

    message

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { router } from './routes/routes';
-import { cors } from './services/cors';
+import { corsmiddleware } from './middlewares/corsmiddleware';
+import cors from 'cors';
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "*");
     res.header("Access-Control-Allow-Headers", "*");
+    app.use(cors())
     next();
 });
 
--- src/middlewares/corsmiddleware.ts
+++ src/middlewares/corsmiddleware.ts
@@ -1,4 +1,4 @@
-export const cors = (req, res, next) => {
+export const corsmiddleware = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://frontend-newsletter-nextjs.vercel.app/");
     res.header("Access-Control-Allow-Methods", "OPTIONS, POST");
     res.header("Access-Control-Allow-Headers", "*");
