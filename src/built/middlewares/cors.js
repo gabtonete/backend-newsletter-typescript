7a97f30fe8987a52adbd5d521a2479aac5e57e25
Gabriel Tonete <gabtopi77@gmail.com>
2022-03-14 17:11:43 -0300

    [Fix] Build erros and cors problems

--- src/built/middlewares/cors.js
+++ src/built/middlewares/cors.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.cors = void 0;
 const cors = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://frontend-newsletter-nextjs.vercel.app/");
+    res.header("Access-Control-Allow-Origin", "https://frontend-newsletter-nextjs.vercel.app");
     res.header("Access-Control-Allow-Headers", "Origin, X-Request-Width, Content-Type, Accept");
     next();
 };
